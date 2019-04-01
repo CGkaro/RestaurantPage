22cb002bb799ff02b2f88e29535daa01f23f8b05
Carlos <practicantes4@lopez-fernandez.com>
2019-03-31 22:11:03 -0700

    added paypal button

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Title } from "../../utils";
 import styled from "styled-components";
 import CartColumns from "./CartColumns";
@@ -7,26 +7,28 @@ import { ProductConsumer } from "../../context";
 import CartList from "./CartList";
 import CartTotals from "./CartTotals";
 
-export default function Cart() {
-  return (
-    <section>
-      <ProductConsumer>
-        {val => {
-          const { cart } = val;
-          if (cart.length > 0) {
-            return (
-              <React.Fragment>
-                <Title title="your" message="cart" />
-                <CartColumns />
-                <CartList value={val} />
-                <CartTotals value={val} />
-              </React.Fragment>
-            );
-          } else {
-            return <EmptyCart />;
-          }
-        }}
-      </ProductConsumer>
-    </section>
-  );
+export default class Cart extends Component {
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {val => {
+            const { cart } = val;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title title="your" message="cart" />
+                  <CartColumns />
+                  <CartList value={val} />
+                  <CartTotals value={val} history={this.props.history} />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    );
+  }
 }
